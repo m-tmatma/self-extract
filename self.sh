ea5085e3ac5396d16e248cf1e843ddf2eb65388d
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-12-26 09:07:54 +0900

    add self-extracting script.

---
+++ self.sh
@@ -0,0 +1,22 @@
+#! /bin/bash -x
+DESTINATION=/tmp/outdir
+
+rm -rf $DESTINATION
+mkdir -p $DESTINATION
+
+ENDLINE=$(grep --line-number --text "^##ENDLINE$" $0 | sed 's/:.*//') 
+tail --lines=+$(($ENDLINE + 1)) $0 \
+| gzip --decompress --to-stdout    \
+| tar --extract --directory $DESTINATION
+
+########## INSTALL_PROCESS ##########
+## ln -sやcpでリソースファイルを設置する.
+## リソースファイルは$DESTINATIONに格納されています。
+#####################################
+
+ls $DESTINATION
+
+rm -rf $DESTINATION
+
+exit
+##ENDLINE
